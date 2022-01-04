ea66029b2a14c983593c7dcf7d8d9cc96417b59a
Carlos Henrique <97001399+CA4L051327@users.noreply.github.com>
2022-01-04 20:52:13 -0300

    Create caracter.py

---
+++ TipodeCaracter/caracter.py
@@ -0,0 +1,11 @@
+n1 = input('Digite qualquer coisa?')
+print('O tipo primitivo é:', type(n1))
+print('Tem espaços?', n1.isspace())
+print('É um numero?', n1.isnumeric())
+print('É alfabetico?', n1.isalpha())
+print('É alfa numerico?', n1.isalnum())
+print('Esta em maisuculo?', n1.isupper())
+print('Esta em minusculo?', n1.islower())
+print('Esta capitalizado?', n1.istitle())
+
+
