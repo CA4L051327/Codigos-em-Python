501498888e9e00e4db3de77d823c47cb47140308
Carlos Henrique <97001399+CA4L051327@users.noreply.github.com>
2022-01-04 20:50:25 -0300

    Update calculo.py

---
+++ Cálculos/calculo.py
@@ -0,0 +1,16 @@
+valor1 = int(input('Digite o  primeiro numero?'))
+valor2 = int(input('Digite o segundo numero?'))
+
+soma = valor1+valor2
+multiplicacao = valor1*valor2
+divisao = valor1/valor2
+subtracao = valor1-valor2
+resto = valor1%valor2
+
+print('A soma entre {} e {} vale {}'.format(valor1, valor2, soma))
+print('A multiplicaçao entre {} e {} vale {}'.format(valor1, valor2, multiplicacao))
+print('A divisao entre {} e {} vale {}'.format(valor1, valor2, divisao))
+print('A subtraçao entre {} e {} vale {}'.format(valor1, valor2, subtracao))
+print('O resto entre {} e {} vale {}'.format(valor1, valor2, resto))
+
+
