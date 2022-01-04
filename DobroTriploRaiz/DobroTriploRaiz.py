5caa750a4420dad39d678da272c3fd7450fba0ed
Carlos Henrique <97001399+CA4L051327@users.noreply.github.com>
2022-01-04 20:55:12 -0300

    Create DobroTriploRaiz.py

---
+++ DobroTriploRaiz/DobroTriploRaiz.py
@@ -0,0 +1,9 @@
+numero = int(input('Digite um numero:'))
+
+dobro = numero * 2
+triplo = numero * 3
+raiz = pow (numero, 1/2)
+
+###raiz pode fazer assim: **(1/2)
+
+print('O dobro é {} o triplo {} e a raiz {:.2f}'.format(dobro, triplo, raiz))
