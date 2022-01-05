525752c8a377c8e1ed8f97d8967fc1cbaa0dadeb
Carlos Henrique <97001399+CA4L051327@users.noreply.github.com>
2022-01-04 21:04:45 -0300

    Create Hipotenusa.py

---
+++ AluguelCarro/Hipotenusa/Hipotenusa.py
@@ -0,0 +1,8 @@
+import math
+
+oposto = float(input('Comprimento cateto oposto:'))
+adjacente = float(input("Comprimento catedo adjacente:"))
+
+print('O comprimento da hipotenusa é {:.2f}'.format(math.hypot(oposto, adjacente)))
+
+##hipotenusa = math.hypot(7, 5)
