9cbb2ed7f92a47caee662e2b8c8679fbef5e7712
Carlos Henrique <97001399+CA4L051327@users.noreply.github.com>
2022-01-04 21:02:37 -0300

    Create AluguelCarros.py

---
+++ AluguelCarro/AluguelCarros.py
@@ -0,0 +1,9 @@
+dias = int(input('Por quantos dias o carro foi alugado?'))
+km = float(input('Quantos km o carro rodou?'))
+
+valor = (dias * 60) + (km * 0.15)
+
+print('O preço total a ser pago pelo aluguel é de {:.2f}'.format(valor))
+
+## 60 por dia
+## 0,15 por km rodado
